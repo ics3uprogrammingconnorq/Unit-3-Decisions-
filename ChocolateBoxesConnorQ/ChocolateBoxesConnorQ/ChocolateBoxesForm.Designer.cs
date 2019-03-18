de8f09b1997429300cca102d303b661678535396
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-03-18 13:11:18 -0400

    chocolate boxes

---
+++ ChocolateBoxesConnorQ/ChocolateBoxesConnorQ/ChocolateBoxesForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ChocolateBoxesConnorQ
+{
+    partial class frmChocolateBoxes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblPrize = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(140, 104);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(70, 155);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblPrize
+            // 
+            this.lblPrize.AutoSize = true;
+            this.lblPrize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrize.Location = new System.Drawing.Point(170, 158);
+            this.lblPrize.Name = "lblPrize";
+            this.lblPrize.Size = new System.Drawing.Size(0, 20);
+            this.lblPrize.TabIndex = 2;
+            // 
+            // frmChocolateBoxes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 283);
+            this.Controls.Add(this.lblPrize);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmChocolateBoxes";
+            this.Text = "Chocolate Boxes by Connor Quinlan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblPrize;
+    }
+}
+
---
+++ ChocolateBoxesConnorQ/ChocolateBoxesConnorQ/ChocolateBoxesForm.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: Day-Month-Year
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #19 - Chocolate Boxes
+ * This program...
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChocolateBoxesConnorQ
+{
+    public partial class frmChocolateBoxes : Form
+    {
+
+        int boxesSold;
+
+        public frmChocolateBoxes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            boxesSold = int.Parse(txtInput.Text);
+
+            if (boxesSold > 20)
+            {
+                lblPrize.Text = "Prize!";
+            }
+            else
+            {
+                if (boxesSold > 10)
+                {
+                    lblPrize.Text = "Honorable Mention";
+                }
+                else
+                {
+                    lblPrize.Text = "Small Prize";
+                }
+            }
+        }
+    }
+}
