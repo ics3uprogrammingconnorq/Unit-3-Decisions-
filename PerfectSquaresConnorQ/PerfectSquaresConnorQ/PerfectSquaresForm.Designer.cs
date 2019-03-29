dffabcad75351157b404383dd6e108f4636ec7e5
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-03-28 20:26:07 -0400

    perfect squares

---
+++ PerfectSquaresConnorQ/PerfectSquaresConnorQ/PerfectSquaresForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PerfectSquaresConnorQ
+{
+    partial class frmPerfectSquares
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudInput = new System.Windows.Forms.NumericUpDown();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(355, 48);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 30);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudInput
+            // 
+            this.nudInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudInput.Location = new System.Drawing.Point(335, 123);
+            this.nudInput.Name = "nudInput";
+            this.nudInput.Size = new System.Drawing.Size(120, 26);
+            this.nudInput.TabIndex = 1;
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 20;
+            this.lstOutput.Location = new System.Drawing.Point(206, 194);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(391, 184);
+            this.lstOutput.TabIndex = 2;
+            // 
+            // frmPerfectSquares
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.nudInput);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmPerfectSquares";
+            this.Text = "Perfect Squares by Connor Quinlan";
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudInput;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
---
+++ PerfectSquaresConnorQ/PerfectSquaresConnorQ/PerfectSquaresForm.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 28-03-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #22 - Perfect Squares
+ * This program calculates all perfect squares up to a given value.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PerfectSquaresConnorQ
+{
+    public partial class frmPerfectSquares : Form
+    {
+        public frmPerfectSquares()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            int endingValue;
+            int value;
+            Double squareRootAsDouble;
+            int squareRootAsInteger;
+
+            // set the initial values
+            value = 1;
+
+            // clear all items from the list box
+            lstOutput.Items.Clear();
+
+            // get the user's end value selection from the number up-down box
+            endingValue = Convert.ToInt32(this.nudInput.Value);
+
+            // continue starting any perfect squares between the minimum value and the user's selected end value
+            while (value <= endingValue)
+            {
+                // take the square root of the value
+                squareRootAsDouble = Math.Sqrt(value);
+
+                // convert the double to an integer
+                squareRootAsInteger = Convert.ToInt32(squareRootAsDouble);
+
+                // the only way for both the decimal and the integer to be equal is if the value was a perfect square
+                if (squareRootAsInteger == squareRootAsDouble)
+                {
+                    this.lstOutput.Items.Add(value + " is a perfect square");
+                    this.Refresh();
+                }
+
+                // increament counter
+                value++;
+
+            }
+        }
+    }
+}
