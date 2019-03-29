91ea5d2a900b8aa2cd010fda25217c73fdc8e880
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-03-28 21:11:47 -0400

    sum of numbers

---
+++ SumOfNumbersConnorQ/SumOfNumbersConnorQ/SumOfNumbersForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SumOfNumbersConnorQ
+{
+    partial class frmSumOfNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(343, 110);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 26);
+            this.txtInput.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(355, 59);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 32);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 20;
+            this.lstNumbers.Location = new System.Drawing.Point(181, 181);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(440, 224);
+            this.lstNumbers.TabIndex = 2;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(226, 424);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 24);
+            this.lblAnswer.TabIndex = 3;
+            // 
+            // frmSumOfNumbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 466);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmSumOfNumbers";
+            this.Text = "Sum of Numbers by Connor Quinlan";
+            this.Load += new System.EventHandler(this.frmSumOfNumbers_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ SumOfNumbersConnorQ/SumOfNumbersConnorQ/SumOfNumbersForm.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 28-03-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #23 - Sum of Numbers
+ * This program the calculates the sum of numbers up to a value.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumOfNumbersConnorQ
+{
+    public partial class frmSumOfNumbers : Form
+    {
+        public frmSumOfNumbers()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSumOfNumbers_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            Double sumAnswer;
+            Double sumNumber;
+            int counter;
+
+            // clear the items from the listbox
+            this.lstNumbers.Items.Clear();
+
+            // initialize the final answer to 1
+            sumAnswer = 1;
+
+            // get the number from the user
+            sumNumber = Convert.ToDouble(this.txtInput.Text);
+
+            for (counter = 1; counter <= sumNumber; counter++)
+            {
+                // list the counter number in the listbox for the user to see
+                this.lstNumbers.Items.Add(counter);
+
+                // add the counter to the answer
+                sumAnswer = sumAnswer + counter;
+            }
+
+            // convert the sumAnswer to a string and insert it in the label to display the answer
+            lblAnswer.Text = "The sum of numbers up to " + this.txtInput.Text + " is " + Convert.ToString(sumAnswer - 1);
+        }
+    }
+}
