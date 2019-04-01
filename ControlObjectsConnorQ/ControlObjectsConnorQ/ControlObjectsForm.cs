a61d8ed36c7195c60b2f4fc2164f9332e69cac86
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-04-01 13:10:26 -0400

    control objects

---
+++ ControlObjectsConnorQ/ControlObjectsConnorQ/ControlObjectsForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ControlObjectsConnorQ
+{
+    public partial class frmControlObjects : Form
+    {
+        public frmControlObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Blue;
+
+                if (aControlObject.GetType() == typeof(Label))
+                {
+                    Thread.Sleep(200);
+                    aControlObject.BackColor = Color.Orange;
+                }
+            }
+        }
+    }
+}
