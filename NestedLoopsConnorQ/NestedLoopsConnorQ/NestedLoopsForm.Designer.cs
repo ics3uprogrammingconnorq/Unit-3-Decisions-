95180b0b4719344d4e9ce1fd8353ad462e85e056
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-04-15 12:14:32 -0400

    nested

---
+++ NestedLoopsConnorQ/NestedLoopsConnorQ/NestedLoopsForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace NestedLoopsConnorQ
+{
+    partial class frmNestedLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(190, 53);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(81, 28);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 20;
+            this.lstOutput.Location = new System.Drawing.Point(140, 118);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(182, 124);
+            this.lstOutput.TabIndex = 1;
+            // 
+            // frmNestedLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 274);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmNestedLoops";
+            this.Text = "Nested Loops by Connor Quinlan";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
---
+++ NestedLoopsConnorQ/NestedLoopsConnorQ/NestedLoopsForm.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: April 4th 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #26 - Nested Loops
+ * This program uses nested loops to convert capital letters to lowercase.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NestedLoopsConnorQ
+{
+    public partial class frmNestedLoops : Form
+    {
+        public frmNestedLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Declare my variables
+            string capital;
+            string lowercase;
+
+            int counter1;
+            int counter2;
+
+            //Clears items in the listbox
+            lstOutput.Items.Clear();
+
+            for (counter1 = 65; counter1 <= 90; counter1++)
+            {
+                //Convert counter to its capital letter in unicode
+                capital = Char.ConvertFromUtf32(counter1);
+
+                for (counter2 = 97; counter2 <= 122; counter2++)
+                {
+                    //Convert counter to its lowercase letter in unicode
+                    lowercase = Char.ConvertFromUtf32(counter2);
+
+                    //Displays the converted letters in the listbox
+                    lstOutput.Items.Add(capital + "-> " + lowercase);
+                }
+            }
+        }
+    }
+}
